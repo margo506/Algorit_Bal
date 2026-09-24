a49ec15a8a9c23fd4832da789da43b64972a63bb
Margo Bal <bal.m.v_ipz24@rcit.ukr.education>
2026-09-24 22:00:17 +0300

    Task3

--- Lab3/Task3/Program.cs
+++ Lab3/Task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        int N = 5;
+        string[] eventsArray = new string[N];
+        int writeIndex = 0;
+        int count = 0;
+        string[] stream =
+        {
+            "Подія1",
+            "Подія2",
+            "Подія3",
+            "Подія4",
+            "Подія5",
+            "Подія6",
+            "Подія7"
+        };
+        foreach (string e in stream)
+        {
+            eventsArray[writeIndex] = e;
+            writeIndex = (writeIndex + 1) % N;
+            if (count < N)
+                count++;
+        }
+        Console.InputEncoding = Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Останні події:");
+        int start = (count == N) ? writeIndex : 0;
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(eventsArray[(start + i) % N]);
+        }
+    }
+}
--- Lab3/Task3/obj/Debug/net9.0/Task3.AssemblyInfo.cs
+++ Lab3/Task3/obj/Debug/net9.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1f33d6f11cf232b424f124238c8e553fc51915")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
