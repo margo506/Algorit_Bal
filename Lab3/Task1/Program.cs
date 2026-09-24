7385352fc15545e72c699f96a621114bf3cc78bb
Margo Bal <bal.m.v_ipz24@rcit.ukr.education>
2026-09-24 21:51:15 +0300

    Task1

---
+++ Lab3/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        List<string> journal = new List<string>();
+        Console.InputEncoding = Console.OutputEncoding = Encoding.UTF8;
+        journal.Add("Вхід у систему");
+        journal.Add("Відкрито документ");
+        journal.Add("Збережено зміни");
+        journal.Add("Вихід");
+        Console.WriteLine("Журнал у зворотному порядку:");
+        for (int i = journal.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(journal[i]);
+        }
+    }
+}
